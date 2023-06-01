0defee8c330ac221379142d71bea26cbd4ca7c66
MaxSlobodynuk <119804658+MaxSlobodynuk@users.noreply.github.com>
2023-06-01 12:56:40 +0000

    Deploying to gh-pages from @ MaxSlobodynuk/goit-js-hw-09@4e7cb6685a91fa6cb7eaf2ea9aaaf285ca3cdaa3 🚀

--- 01-color-switcher.a77ea463.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,colorChangeInterval=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(colorChangeInterval),t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.a77ea463.js.map

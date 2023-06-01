4e7cb6685a91fa6cb7eaf2ea9aaaf285ca3cdaa3
Max <slobodynuk.777@gmail.com>
2023-06-01 15:44:56 +0300

    goit-js-hw-09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,23 +1,25 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
-
+let colorChangeInterval;
 
 startBtn.addEventListener('click', startColorChange);
 stopBtn.addEventListener('click', stopColorChange);
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-
 function startColorChange() {
-    startBtn.disabled = true;
     colorChangeInterval = setInterval(() => {
         document.body.style.backgroundColor = getRandomHexColor();
     }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
 }
 
 function stopColorChange() {
     clearInterval(colorChangeInterval);
     startBtn.disabled = false;
+    stopBtn.disabled = true;
 }
+
